d656e47bed5b3fe16a36c048321b494370be215a
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-09-22 17:29:39 -0300

    reconstruccion de mallas

--- backend/src/syllabus/syllabus.procesador.service.ts
+++ backend/src/syllabus/syllabus.procesador.service.ts
@@ -10,14 +10,45 @@ export class ProcesadorService {
       return { status: 'error', message: data.error };
     }
 
-    // Ejemplo de procesamiento
-    const ramos = data.map(r => ({
-      asignatura: r.asignatura
-    }));
+    // Algoritmo para reconstruir la malla
+    const niveles = data.reduce((actual , max) => actual.nivel > max.nivel ? actual : max, -1).nivel;
+
+    const malla: any[] = [];
+
+    for (let index = 0; index < niveles; index++) {
+      const ramosDelSemestre = data
+        .filter(r => r.nivel == index +1)
+        .map(r => ({
+          asignatura: r.asignatura,
+          prereq: r.prereq ? r.prereq.split(",") : []
+        }));
+
+        const ramosConPrer = ramosDelSemestre.map(ramo => ({
+          asignatura: ramo.asignatura,
+          prerequisitos: ramo.prereq.map(p => {
+            const encontrado = data.find(r => r.codigo === p);
+            return encontrado ? encontrado.asignatura : null
+          }).filter(r => r != null)
+
+        }));
+
+
+      malla.push({
+        semestre: index +1,
+        ramos: ramosConPrer
+      });
+
+      
+    }
+
+
+    //const mallaFinal = malla.map(sem => (sem.ramos.prereq.map(p => data.filter(r => r.codigo == p).map(n => n.asignatura))))
 
     return {
       status: 'ok',
-      ramos
+      malla
     };
   }
+
+
 }
