f4c0aad5e8a181f9384b197ae933a8edbb2a5bb4
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-12-17 16:05:20 -0300

    dashboard ahora si

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -3,10 +3,10 @@ import { useCallback } from "react";
 export const useApi = () => {
 
   // Obtener endpoints
-  //const getBaseUrl = useCallback(() => 'https://codsylla.onrender.com', []);
+  const getBaseUrl = useCallback(() => 'https://codsylla.onrender.com', []);
   //const getBaseUrl = useCallback(() => 'https://bg6nj47p-3001.brs.devtunnels.ms', []);
   //const getBaseUrl = useCallback(() => 'https://sb51xcj9-3001.brs.devtunnels.ms', []);
-  const getBaseUrl = useCallback(() => 'http://localhost:3001', []);
+  //const getBaseUrl = useCallback(() => 'http://localhost:3001', []);
 
   return { getBaseUrl };
 };
